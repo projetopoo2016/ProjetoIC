d343204e0962c863c3398827f37a151d1ce6d277
Marco Neves <marco.neves@universidadeeuropeia.pt>
2018-03-22 11:51:38 +0000

    Node start correction

--- public/js/resultados.js
+++ public/js/resultados.js
@@ -4,58 +4,47 @@
         // Material Select Initialization
         $(document).ready(function () {
             $('.mdb-select').material_select();
+
+            var ctxB = document.getElementById("barChart").getContext('2d');
+            var myBarChart = new Chart(ctxB, {
+                type: 'bar',
+                data: {
+                    labels: ["January", "Febuary", "March", "April", "May"],
+                    datasets: [{
+                        label: '# of Votes',
+                        data: [13, 19, 8, 11, 5],
+                        backgroundColor: [
+                            'rgba(255, 99, 132, 0.3)',
+                            'rgba(41, 182, 246, 0.3)',
+                            'rgba(255, 187, 51, 0.3)',
+                            'rgba(66, 133, 244, 0.3)',
+                            'rgba(153, 102, 255, 0.3)',
+    
+                        ],
+                        borderColor: [
+                            'rgba(255,99,132,1)',
+                            'rgba(41, 182, 246, 1)',
+                            'rgba(255, 187, 51, 1)',
+                            'rgba(66, 133, 244, 1)',
+                            'rgba(153, 102, 255, 1)',
+    
+                        ],
+                        borderWidth: 2
+                    }]
+                },
+                optionss: {
+                    scales: {
+                        yAxes: [{
+                            ticks: {
+                                beginAtZero: true
+                            }
+                        }]
+                    }
+                }
+            });
         });
 
         // Tooltips Initialization
         $(function () {
             $('[data-toggle="tooltip"]').tooltip()
         })
-    
-        // Small chart
-        $(function () {
-            $('.min-chart#chart-sales').easyPieChart({
-                barColor: "#4285F4",
-                onStep: function (from, to, percent) {
-                    $(this.el).find('.percent').text(Math.round(percent));
-                }
-            });
-        });
-
-        //bar
-        var ctxB = document.getElementById("barChart").getContext('2d');
-        var myBarChart = new Chart(ctxB, {
-            type: 'bar',
-            data: {
-                labels: ["January", "Febuary", "March", "April", "May"],
-                datasets: [{
-                    label: '# of Votes',
-                    data: [13, 19, 8, 11, 5],
-                    backgroundColor: [
-                        'rgba(255, 99, 132, 0.3)',
-                        'rgba(41, 182, 246, 0.3)',
-                        'rgba(255, 187, 51, 0.3)',
-                        'rgba(66, 133, 244, 0.3)',
-                        'rgba(153, 102, 255, 0.3)',
-
-                    ],
-                    borderColor: [
-                        'rgba(255,99,132,1)',
-                        'rgba(41, 182, 246, 1)',
-                        'rgba(255, 187, 51, 1)',
-                        'rgba(66, 133, 244, 1)',
-                        'rgba(153, 102, 255, 1)',
-
-                    ],
-                    borderWidth: 2
-                }]
-            },
-            optionss: {
-                scales: {
-                    yAxes: [{
-                        ticks: {
-                            beginAtZero: true
-                        }
-                    }]
-                }
-            }
-        });
